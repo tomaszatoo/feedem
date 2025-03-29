a9f06a76a23dee537a1678a9dc2426ef39662a0d
Tomáš Javůrek <xvjavurek@vutbr.cz>
2025-03-29 11:27:00 +0100

    add not-found component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,11 @@ import { Routes } from '@angular/router';
 import { MainMenuComponent } from './components/main-menu/main-menu.component';
 import { SettingsComponent } from './components/settings/settings.component';
 import { GameComponent } from './components/game/game.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 export const routes: Routes = [
     { path: '', component: MainMenuComponent },
     { path: 'settings', component: SettingsComponent },
     { path: 'game', component: GameComponent },
-    { path: '**', redirectTo: '' }
+    { path: '**', component: NotFoundComponent }
 ];
