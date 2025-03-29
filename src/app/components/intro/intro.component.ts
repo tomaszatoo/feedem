553f3f4cfca47b488c6bfafd0a1be3c53b06a716
Tomáš Javůrek <xvjavurek@vutbr.cz>
2025-03-29 11:49:18 +0100

    init intro component

---
+++ src/app/components/intro/intro.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-intro',
+  imports: [],
+  templateUrl: './intro.component.html',
+  styleUrl: './intro.component.scss'
+})
+export class IntroComponent {
+
+}
