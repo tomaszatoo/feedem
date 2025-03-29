937a2e3a2000ae62ffbc85785692792571d0cfb3
Tomáš Javůrek <xvjavurek@vutbr.cz>
2025-03-29 11:49:38 +0100

    create and style main menu

--- src/app/components/main-menu/main-menu.component.ts
+++ src/app/components/main-menu/main-menu.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-main-menu',
-  imports: [],
+  imports: [ RouterLink ],
   templateUrl: './main-menu.component.html',
   styleUrl: './main-menu.component.scss'
 })
