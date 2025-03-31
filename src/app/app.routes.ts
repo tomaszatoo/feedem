94fc23d99c4ea85a1e39ee743102bfd77267d8c9
Tomáš Javůrek <xvjavurek@vutbr.cz>
2025-03-31 18:20:46 +0200

    init controller component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,10 +4,12 @@ import { MainMenuComponent } from './components/main-menu/main-menu.component';
 import { SettingsComponent } from './components/settings/settings.component';
 import { GameComponent } from './components/game/game.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
+import { ControllerComponent } from './components/controller/controller.component';
 
 export const routes: Routes = [
     { path: '', component: MainMenuComponent },
     { path: 'settings', component: SettingsComponent },
     { path: 'game', component: GameComponent },
+    { path: 'controller', component: ControllerComponent },
     { path: '**', component: NotFoundComponent }
 ];
---
+++ src/app/components/controller/controller.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-controller',
+  imports: [],
+  templateUrl: './controller.component.html',
+  styleUrl: './controller.component.scss'
+})
+export class ControllerComponent {
+
+}
