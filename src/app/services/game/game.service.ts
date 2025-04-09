f7bb039472cd8100c3bbd926c21c8f4928d2f80c
Tomáš Javůrek <xvjavurek@vutbr.cz>
2025-04-09 21:18:54 +0200

    add game subject

--- src/app/services/game/game.service.ts
+++ src/app/services/game/game.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+// models
+import { Game } from '../../models/game';
+// rxjs
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GameService {
+
+  game: Subject<Game> = new Subject();
 
   constructor() { }
 }
